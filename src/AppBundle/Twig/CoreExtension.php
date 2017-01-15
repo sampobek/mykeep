4d3f14f408671704fb453ffedd99c7c9a07bd65a
Zamirbek Abdyldaev <sampobek@gmail.com>
2017-01-15 22:09:53 +0600

    #2 Installed Bulma

---
+++ src/AppBundle/Twig/CoreExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Twig;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CoreExtension extends \Twig_Extension
+{
+    protected $container;
+
+    protected $request;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->request = $container->get('request_stack')->getCurrentRequest();
+    }
+
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('active', array($this, 'isActive')),
+        );
+    }
+
+    public function isActive($route, $class) {
+        if ($this->request->get('_route') == $route) {
+            return $class;
+        }
+
+        return '';
+    }
+
+    public function getName()
+    {
+        return 'core_extension';
+    }
+}
