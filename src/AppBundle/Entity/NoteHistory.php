4ac0b28bcd3b9b91af4b90588327a4192abe9290
Zamirbek Abdyldaev <sampobek@gmail.com>
2017-01-18 00:23:55 +0600

    #9 Note History

---
+++ src/AppBundle/Entity/NoteHistory.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * NoteHistory
+ *
+ * @ORM\Table(name="note_history")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NoteHistoryRepository")
+ */
+class NoteHistory
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+    /**
+     * Many NoteHistories related to one Note.
+     * @ORM\ManyToOne(targetEntity="Note")
+     * @ORM\JoinColumn(name="note_id", referencedColumnName="id", nullable=false)
+     */
+    private $note;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return NoteHistory
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return NoteHistory
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return NoteHistory
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set note
+     *
+     * @param \AppBundle\Entity\Note $note
+     *
+     * @return NoteHistory
+     */
+    public function setNote(\AppBundle\Entity\Note $note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return \AppBundle\Entity\Note
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+}
--- src/AppBundle/Listener/NoteListener.php
+++ src/AppBundle/Listener/NoteListener.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Listener;
 
 use AppBundle\Entity\Note;
+use AppBundle\Entity\NoteHistory;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PostFlushEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -10,8 +13,26 @@ class NoteListener
 {
     private $container;
 
+    private $noteHistories = [];
+
     public function __construct(ContainerInterface $container) {
         $this->container = $container;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if (!$entity instanceof Note) {
+            return;
+        }
+
+        $noteHistory = new NoteHistory();
+        $noteHistory->setTitle($entity->getTitle());
+        $noteHistory->setContent($entity->getContent());
+        $noteHistory->setNote($entity);
+        $noteHistory->setCreatedAt($entity->getCreatedAt());
+        $this->noteHistories[] = $noteHistory;
     }
 
     public function preUpdate(PreUpdateEventArgs $args)
@@ -24,6 +45,28 @@ class NoteListener
 
         if ($args->hasChangedField('title') || $args->hasChangedField('content')) {
             $entity->setUpdatedAt(new \DateTime());
+
+            $noteHistory = new NoteHistory();
+            $noteHistory->setTitle($entity->getTitle());
+            $noteHistory->setContent($entity->getContent());
+            $noteHistory->setNote($entity);
+            $noteHistory->setCreatedAt($entity->getUpdatedAt());
+            $this->noteHistories[] = $noteHistory;
+        }
+    }
+
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        $em = $args->getEntityManager();
+
+        if (!empty($this->noteHistories)) {
+            foreach ($this->noteHistories as $noteHistory) {
+                if ($noteHistory instanceof NoteHistory) {
+                    $em->persist($noteHistory);
+                }
+            }
+            $this->noteHistories = [];
+            $em->flush();
         }
 
     }
